7cca972f47518250e1452efab82cce3fe2a7c56e
Antony Cuero <thejunianthony@hotmail.com>
2017-12-16 14:05:37 -0300

    editar venta

--- scripts/controller/venta.js
+++ scripts/controller/venta.js
@@ -2,9 +2,10 @@
     'use strict';
     angular.module('controller')
         .controller('venta', venta);
-    function venta($http, LxNotificationService, $state, localStorageService) {
+    function venta($scope, $http, LxNotificationService, $state, localStorageService) {
         let vm = this;
         vm.usuario = localStorageService.get('me');
+        vm.edit = editarProducto;
         vm.cerrar = cerrarSession;
         vm.add = anadirProducto;
         vm.sell = generarVenta;
@@ -13,26 +14,65 @@
         $http.get('http://localhost:8585/api/productos').then(response => {
             vm.products = response.data;
         })
+        $http.get('http://localhost:8585/api/ventas').then(response => {
+            vm.dataTableTbodyVenta = response.data.filter(el => {
+                return el.usuario && el.usuario !== null && el.usuario._id === vm.usuario._id && new Date(el.createdAt).toLocaleDateString() === new Date().toLocaleDateString();
+            }).map(el => {
+                return {
+                    _id: el._id,
+                    productos: el.productos,
+                    hora: new Date(el.createdAt).getHours() + ':' + new Date(el.createdAt).getMinutes(),
+                    total: el.productos.sum((item) => {
+                        return item.producto.precio * item.cantidad;
+                    }),
+                }
+            });
+        })
         vm.dataTableThead = [
             { name: 'nombre', label: 'Producto' },
             { name: 'cantidad', label: 'Cantidad' }
+        ]
+        vm.dataTableTheadVenta = [
+            { name: 'hora', label: 'Hora' },
+            { name: 'total', label: 'Total' },
         ]
         vm.dataTableTbody = []
         function inicio() {
             $state.go('app');
         }
         function generarVenta() {
-            $http({
-                method: 'POST', url: 'http://localhost:8585/api/venta',
-                data: {
-                    productos: angular.copy(vm.dataTableTbody),
-                    usuario: localStorageService.get('me')._id
-                }
-            }).then(response => {
-                LxNotificationService.success('Venta Generada!');
-                vm.dataTableTbody = []
-            });
-            vm.total = 0;
+            if (vm.selectedRows.length >= 1) {
+                $http({
+                    method: 'PUT', url: 'http://localhost:8585/api/venta/' + vm.selectedRows[0]._id,
+                    data: {
+                        productos: angular.copy(vm.dataTableTbody)
+                    }
+                }).then(response => {
+                    LxNotificationService.success('Venta Actualizada!');
+                    vm.dataTableTbodyVenta = vm.dataTableTbodyVenta.map(el => {
+                        if (response.data._id === el._id)
+                            el.total = vm.dataTableTbody.sum((item) => {
+                                return item.valor * item.cantidad;
+                            })
+                        return el
+                    })
+                    vm.dataTableTbody = []
+                    vm.total = 0;
+                    vm.selectedRows = []
+                });
+            } else {
+                $http({
+                    method: 'POST', url: 'http://localhost:8585/api/venta',
+                    data: {
+                        productos: angular.copy(vm.dataTableTbody),
+                        usuario: localStorageService.get('me')._id
+                    }
+                }).then(response => {
+                    LxNotificationService.success('Venta Generada!');
+                    vm.dataTableTbody = []
+                });
+                vm.total = 0;
+            }
         }
         function anadirProducto(p, value) {
             if (vm.dataTableTbody.find(producto => { return producto.producto === p._id })) {
@@ -50,7 +90,26 @@
                 return producto.valor * producto.cantidad;
             });
         }
-        function cerrarSession(){
+        function editarProducto() {
+            vm.dataTableTbody = vm.selectedRows[0].productos.map(el => {
+                return { producto: el.producto._id, cantidad: el.cantidad, nombre: el.producto.nombre, valor: el.producto.precio }
+            })
+            vm.total = vm.dataTableTbody.sum(function (producto) {
+                return producto.valor * producto.cantidad;
+            });
+        }
+        $scope.$on('lx-data-table__selected', actualizarTabla);
+        $scope.$on('lx-data-table__unselected', actualizarTabla);
+        function actualizarTabla(_event, _dataTableId, _selectedRows) {
+            if (_dataTableId === "ventas") {
+                if (_selectedRows.length === 1) {
+                    vm.selectedRows = _selectedRows;
+                } else if (_selectedRows.length > 1) {
+                    vm.selectedRows = [_selectedRows.reverse()[0]]
+                }
+            }
+        }
+        function cerrarSession() {
             localStorageService.remove('me')
             $state.go('login')
         }
